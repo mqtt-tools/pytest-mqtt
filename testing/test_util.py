1b8e426d522635c74cc42f74ba1de5bed8f066ee
Andreas Motl <andreas.motl@panodata.org>
2025-03-16 19:28:19 +0100

    Chore: Improve stability of `probe_tcp_connect` test cases

--- testing/test_util.py
+++ testing/test_util.py
@@ -1,9 +1,9 @@
 from pytest_mqtt.util import probe_tcp_connect
 
 
-def test_probe_tcp_connect_available():
-    assert probe_tcp_connect("9.9.9.9", 443) is True
+def test_probe_tcp_connect_available(httpserver):
+    assert probe_tcp_connect(httpserver.host, httpserver.port) is True
 
 
 def test_probe_tcp_connect_unavailable():
-    assert probe_tcp_connect("9.9.9.9", 444) is False
+    assert probe_tcp_connect("localhost", 12345) is False
